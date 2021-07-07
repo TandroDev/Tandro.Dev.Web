9f0061592bc5c57711f016c00a2e216bc166d1da
Tobias Hungwe <tobias.hungwe@student.howest.be>
2021-07-07 12:02:39 +0200

    clean up project

--- Tandro.Dev.Web/Models/ErrorViewModel.cs
+++ Tandro.Dev.Web/Models/ErrorViewModel.cs
@@ -6,6 +6,6 @@ namespace Tandro.Dev.Web.Models
     {
         public string RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
